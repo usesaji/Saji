19122dd48ec5d03848d9c8ee3027e292f480c911
Tubolayefa <tlayefaah@gmail.com>
2026-07-06 14:50:42 +0100

    about page

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -47,14 +47,15 @@ const About = () => {
 				<Stats />
 			</div>
 
-			<div className="custom-container">
-				<div className="rounded-[20px] bg-neutral-comment sm:flex lg:w-8/12 sm:justify-between sm:gap-13.25 pt-8.75 px-5.5 sm:pt-14.5 sm:pl-14 sm:pr-10.5 sm:rounded-[30px] max-sm:max-h-130.25 max-h-125.25 overflow-hidden">
+			<div className="custom-container grid grid-cols-1 lg:grid-cols-12 gap-x-6 gap-y-5 sm:gap-y-7.5">
+				{/* Build Your Circle */}
+				<div className="rounded-[20px] bg-neutral-comment sm:flex lg:col-span-7 sm:justify-between gap-6 xl:gap-0 pt-8.75 px-5.5 sm:pt-14.5 md:pl-10  xl:pl-14 sm:pr-8 sm:rounded-[30px] max-sm:max-h-130.25 max-h-110.25 overflow-hidden">
 					<div className="sm:w-1/2 flex flex-col justify-between pb-10">
 						<div>
 							<h3 className="text-primary-dark-active text-[32px] md:text-[42px] leading-[99%]">
 								Build Your Circle In Minutes
 							</h3>
-							<p className="mt-4 sm:mt-5">
+							<p className="mt-4 sm:mt-5 w-4/5">
 								Contribute your way, see who&apos;s paid, and receive your
 								payout
 							</p>
@@ -73,9 +74,93 @@ const About = () => {
 						/>
 					</div>
 				</div>
-				<div>1</div>
-				<div>1</div>
-				<div>1</div>
+				{/* Withdraw in minutes */}
+				<div className="rounded-[20px] bg-neutral-comment flex flex-col lg:col-span-5 sm:justify-between pt-8.75 sm:pt-14.5 sm:pl-14 sm:pr-8 sm:rounded-[30px] max-sm:max-h-130.25 max-h-110.25 overflow-hidden">
+					<div className="flex flex-col justify-between pb-5 sm:pb-10 px-5.5">
+						<div>
+							<h3 className="text-primary-dark-active text-[32px] md:text-[42px] leading-[99%]">
+								Withdraw In Minutes
+							</h3>
+							<p className="mt-4 sm:mt-5 w-2/3">
+								Contribute your way, see who&apos;s paid, and receive your
+								payout
+							</p>
+						</div>
+					</div>
+					<div className="flex items-end relative justify-between max-sm:flex-col max-sm:items-start">
+						<Button
+							variant="default"
+							className="sm:mt-5 mb-10 w-fit max-sm:mx-5.5 "
+						>
+							Get Started
+						</Button>
+						<div className="max-sm:mx-auto max-sm:-mb-6 max-sm:justify-self-centers lg:absolute right-0 -bottom-1 lg:right-[-50%] h-60 lg:h-50 xl:h-63.5">
+							<Image
+								alt="Withdraw in minutes"
+								src="/images/about/withdraw.png"
+								height={500}
+								width={500}
+								className="object-contain h-full w-full"
+							/>
+						</div>
+					</div>
+				</div>
+
+				{/* Backed by Blockchain */}
+				<div className="rounded-[20px] bg-neutral-comment flex flex-col lg:col-span-5 sm:justify-between pt-8.75 sm:pt-10 sm:pr-8 sm:rounded-[30px] max-sm:max-h-130.25 max-h-110.25 overflow-hidden">
+					<div className="flex flex-col justify-between pb-5 sm:pb-10 px-5.5">
+						<div>
+							<h3 className="text-primary-dark-active text-[32px] md:text-[42px] leading-[99%]">
+								Backed by blockchain.
+							</h3>
+							<p className="mt-4 sm:mt-3">
+								Every contribution recorded. Every payout automated. No
+								coordinator to trust.
+							</p>
+						</div>
+					</div>
+					<div className="flex items-end relative justify-between max-sm:flex-col max-sm:items-start">
+						<Button variant="default" className="sm:mt-5 mb-10 w-fit mx-5.5 ">
+							Get Started
+						</Button>
+						<div className="max-sm:mx-auto max-sm:-mb-6 max-sm:justify-self-centers lg:absolute right-0 -bottom-10 lg:right-[-10%] h-60 lg:h-50 xl:h-63.5">
+							<Image
+								alt="Backed By Blockchain"
+								src="/images/about/backed.png"
+								height={500}
+								width={500}
+								className="object-contain h-full w-full"
+							/>
+						</div>
+					</div>
+				</div>
+
+				{/* Saving African Style */}
+				<div className="rounded-[20px] bg-neutral-comment sm:flex lg:col-span-7 sm:justify-between gap-6 xl:gap-0 pt-8.75 px-5.5 sm:pt-14.5 md:pl-10  xl:pl-14 sm:pr-8 sm:rounded-[30px] max-sm:max-h-130.25 max-h-110.25 overflow-hidden">
+					<div className="sm:w-1/2 flex flex-col justify-between pb-10">
+						<div>
+							<h3 className="text-primary-dark-active text-[32px] md:text-[42px] leading-[99%]">
+								Save the west African Way
+							</h3>
+							<p className="mt-4 sm:mt-5 w-4/5">
+								Contribute your way, see who&apos;s paid, and receive your
+								payout
+							</p>
+						</div>
+						<Button variant="dark" className="mt-5">
+							Learn More
+						</Button>
+					</div>
+					<div className="sm:w-1/2">
+						<Image
+							alt="Save the african style"
+							src="/images/about/native-drum.png"
+							height={500}
+							width={500}
+							className="object-contain h-full w-full"
+						/>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
