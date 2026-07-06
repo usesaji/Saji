5e22a77f35a0405a8137ef19c1fbc2f181549786
Tubolayefa <tlayefaah@gmail.com>
2026-07-06 12:09:47 +0100

    added first about card

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import { Button } from "../ui/button";
 
 const Stats = () => {
 	const stats = [
@@ -43,6 +45,37 @@ const About = () => {
 		<section>
 			<div className="custom-container py-13 sm:py-20 lg:py-22">
 				<Stats />
+			</div>
+
+			<div className="custom-container">
+				<div className="rounded-[20px] bg-neutral-comment sm:flex lg:w-8/12 sm:justify-between sm:gap-13.25 pt-8.75 px-5.5 sm:pt-14.5 sm:pl-14 sm:pr-10.5 sm:rounded-[30px] max-sm:max-h-130.25 max-h-125.25 overflow-hidden">
+					<div className="sm:w-1/2 flex flex-col justify-between pb-10">
+						<div>
+							<h3 className="text-primary-dark-active text-[32px] md:text-[42px] leading-[99%]">
+								Build Your Circle In Minutes
+							</h3>
+							<p className="mt-4 sm:mt-5">
+								Contribute your way, see who&apos;s paid, and receive your
+								payout
+							</p>
+						</div>
+						<Button variant="dark" className="mt-5">
+							Learn More
+						</Button>
+					</div>
+					<div className="sm:w-1/2">
+						<Image
+							alt="Build your circle"
+							src="/images/about/build-circle.png"
+							height={500}
+							width={500}
+							className="object-contain h-full w-full"
+						/>
+					</div>
+				</div>
+				<div>1</div>
+				<div>1</div>
+				<div>1</div>
 			</div>
 		</section>
 	);
